e721ff5fc8e0540d030d3d49a2a584a3f5791623
Gleb Polyakov <gleb@nylas.com>
2015-12-18 09:40:22 -0800

    Update threads.js

--- example/routes/threads.js
+++ example/routes/threads.js
@@ -5,6 +5,7 @@ router.get('/top', function (req, res, next) {
     var nylas = Nylas.with(req.query.token);
     nylas.threads.first().then(function(thread) {
       res.render('thread', {
+          thread: thread
     })
     });
 })
