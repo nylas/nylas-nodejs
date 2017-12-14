ca0eea11cf5ccf561e8c4be74d2f40b915be5dc0
Jyrki Laurila <jyrki.laurila@gmail.com>
2017-12-13 18:43:19 -0800

    Fix handling of error when not a JSON request
    When sending raw MIME emails, the `json` request option is `false`, which means that the body is still a stringified JSON instead of an object. This means that when ever there was a failure, the error object never included any information about what happened because it was referencing a `.message` property on a string.

--- src/nylas-connection.js
+++ src/nylas-connection.js
@@ -103,6 +103,9 @@ module.exports = class NylasConnection {
     return new Promise((resolve, reject) => {
       return request(options, (error, response, body = {}) => {
         if (error || response.statusCode > 299) {
+          if (_.isString(body)) {
+            body = JSON.parse(body);
+          }
           if (!error) {
             error = new Error(body.message);
           }
