509306b976b4bf1121dfe3ba671950db8670633c
Cole Furfaro-Strode <colestrode@gmail.com>
2019-09-13 15:10:26 -0400

    makes webhook parsing middleware fully async, adds error handling, sets body parser limits to a more reasonable threshold

--- example/webhooks/index.js
+++ example/webhooks/index.js
@@ -15,13 +15,21 @@ const bodyParser = require('body-parser');
 // signature
 app.use(function(req, res, next) {
   req.rawBody = '';
-  req.on('data', function(chunk) {
-    req.rawBody += chunk;
+  req.on('data', (chunk) => (req.rawBody += chunk));
+  req.on('error', (err) => res.status(500).send('Error parsing body'));
+
+  req.on('end', () => {
+    // because the stream has been consumed, other parsers like bodyParser.json 
+    // cannot stream the request data and will time out so we must explicitly parse the body
+    try {
+      req.body = JSON.parse(req.rawBody);
+      next();
+    } catch (err) {
+      res.status(500).send('Error parsing body');
+    }
   });
-  next();
 });
-app.use(bodyParser.json({ limit: '50000mb' })); // support json encoded bodies
-app.use(bodyParser.urlencoded({ limit: '50000mb', extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true })); // support encoded bodies
 
 app.get('/webhook', function(req, res) {
   // Nylas will check to make sure your webhook is valid by making a GET
