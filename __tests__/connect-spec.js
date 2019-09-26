ae71c880e1241478779be89b6191b7e6e3fccb14
Stacey Rutherford <stacey@nylas.com>
2019-09-25 21:36:51 -0400

    implement /connect/token

--- __tests__/connect-spec.js
+++ __tests__/connect-spec.js
@@ -10,6 +10,7 @@ describe('Connect', () => {
   const imapEmail = 'connect_test@yahoo.com';
   const o365Email = 'connect_test@nylatest.onmicrosoft.com';
   const CLIENT_ID = 'abc';
+  const CLIENT_SECRET = 'xyz';
   const authorizeOptions = ((email, provider, settings) => {
   	return {
 	  	name: name,
@@ -22,6 +23,19 @@ describe('Connect', () => {
   const authorizeJSON = {
   	code: 'bOjq4Wt9ZAlCy0CSbVeDGDQ5PquytC',
   }
+  const tokenJSON = {
+    access_token: 'the-token',
+    account_id: 'the-account-id',
+    billing_state: 'paid',
+    email_address: 'erlich@aviato.com',
+    id: 'the-id',
+    linked_at: 1563496685,
+    name: 'Erlich Bachman',
+    object: 'account',
+    organization_unit: 'folder',
+    provider: 'eas',
+    sync_state: 'running',
+  }
 
   describe('authorize without clientId', () => {
   	beforeEach(() => {
@@ -29,9 +43,12 @@ describe('Connect', () => {
   	});
 
     test('Should throw an error when the clientId is not passed in to Nylas.config()', () => {
-      expect.assertions(1);
+      expect.assertions(2);
       expect(() => {
 	      Nylas.connect.authorize(authorizeOptions(exchangeEmail, 'exchange', { username: exchangeEmail, password: password })).then(resp => done())
+      }).toThrow()
+      expect(() => {
+        Nylas.connect.token(authorizeJSON.code).then(resp => done())
       }).toThrow()
     });
   });
@@ -40,7 +57,7 @@ describe('Connect', () => {
   	beforeEach(() => {
   	  Nylas.config({
   	    clientId: CLIENT_ID,
-  	    clientSecret: 'xyz',
+  	    clientSecret: CLIENT_SECRET,
   	  });  		
   	});
 
@@ -61,6 +78,26 @@ describe('Connect', () => {
           	provider: 'exchange',
           	settings: { username: exchangeEmail, password: password },
           	scopes: scopes,
+          }
+        });
+        done();
+      })
+    });
+
+    test('Should do a POST request to /connect/token', done => {
+      expect.assertions(2);
+      Nylas.connect.connection.request = jest.fn(() =>
+        Promise.resolve(tokenJSON)
+      );
+      Nylas.connect.token(authorizeJSON.code).then(resp => {
+        expect(resp).toEqual(tokenJSON);
+        expect(Nylas.connect.connection.request).toHaveBeenCalledWith({
+          method: 'POST',
+          path: '/connect/token',
+          body: {
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            code: authorizeJSON.code,
           }
         });
         done();
