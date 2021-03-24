0f6782243d1f3ecdf64f64600de31d3cac9897d6
Tranced <terhyang@gmail.com>
2021-03-24 01:53:24 -0700

    Revert "Also pass error message instead of only error status code"
    This reverts commit 189971660e0b1f8573eba4c08ba7cea7c458c6cd.

--- src/nylas-connection.js
+++ src/nylas-connection.js
@@ -159,9 +159,6 @@ module.exports = class NylasConnection {
           }
           if (response.statusCode) {
             error.statusCode = response.statusCode;
-            if(!('message' in error)){
-              error.message = response.message;
-            }
           }
           return reject(error);
         } else {
