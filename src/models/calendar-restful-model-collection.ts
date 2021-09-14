4cd0965ab85b21effefc74d841048b14abd050bc
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2021-09-14 18:14:30 -0400

    Added support for Calendar Availability

--- src/models/calendar-restful-model-collection.ts
+++ src/models/calendar-restful-model-collection.ts
@@ -105,4 +105,65 @@ export default class CalendarRestfulModelCollection extends RestfulModelCollecti
         return Promise.reject(err);
       });
   }
+
+  calendarAvailability(
+    options: {
+      emails: Array<{
+        email: string;
+        name: string;
+      }>;
+      duration: number;
+      interval: number;
+      start_time?: string;
+      startTime?: string;
+      end_time?: string;
+      endTime?: string;
+      free_busy?: Array<{
+        emails: string;
+        object: string;
+        time_slots: Array<{
+          object: string;
+          status: string;
+          start_time: string;
+          end_time: string;
+        }>;
+      }>;
+      open_hours: Array<{
+        emails: string[];
+        days: string[];
+        timezone: string;
+        start: string;
+        end: string;
+        object_type: string;
+      }>;
+    },
+    callback?: (error: Error | null, data?: { [key: string]: any }) => void
+  ) {
+    return this.connection
+      .request({
+        method: 'POST',
+        path: `/calendars/availability/consecutive`,
+        body: {
+          emails: options.emails,
+          duration_minutes: options.duration,
+          interval_minutes: options.interval,
+          start_time: options.startTime || options.start_time,
+          end_time: options.endTime || options.end_time,
+          free_busy: options.free_busy || [],
+          open_hours: options.open_hours,
+        },
+      })
+      .then(json => {
+        if (callback) {
+          callback(null, json);
+        }
+        return Promise.resolve(json);
+      })
+      .catch(err => {
+        if (callback) {
+          callback(err);
+        }
+        return Promise.reject(err);
+      });
+  }
 }
