0c08eba944c2d7c41c2d544df5b19452ff75fa60
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2021-12-15 17:29:50 -0500

    Improve job-status object
    - Add missing optional original_data key
    - Set all values to readOnly, as this endpoint is read only
    - Add isSuccessful helper method

--- src/models/job-status.ts
+++ src/models/job-status.ts
@@ -1,11 +1,18 @@
 import RestfulModel from './restful-model';
 import Attributes from './attributes';
+import Message from './message';
 
 export default class JobStatus extends RestfulModel {
   action?: string;
   createdAt?: Date;
   jobStatusId?: string;
   status?: string;
+  originalData?: Message;
+
+  // Returns the status of a job as a boolean
+  isSuccessful(): boolean {
+    return this.status === "successful";
+  }
 }
 
 JobStatus.collectionName = 'job-statuses';
@@ -13,16 +20,25 @@ JobStatus.attributes = {
   ...RestfulModel.attributes,
   action: Attributes.String({
     modelKey: 'action',
+    readOnly: true
   }),
   createdAt: Attributes.DateTime({
     modelKey: 'createdAt',
     jsonKey: 'created_at',
+    readOnly: true
   }),
   jobStatusId: Attributes.String({
     modelKey: 'jobStatusId',
     jsonKey: 'job_status_id',
+    readOnly: true
   }),
   status: Attributes.String({
     modelKey: 'status',
+    readOnly: true
+  }),
+  originalData: Attributes.Object({
+    modelKey: 'originalData',
+    jsonKey: 'original_data',
+    readOnly: true
   }),
 };
