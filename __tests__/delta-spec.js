c68798a45d43b3c0a653c6c90aa97b8e8c14333c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-01-28 15:21:30 -0500

    Update tests to reflect new Delta model

--- __tests__/delta-spec.js
+++ __tests__/delta-spec.js
@@ -1,8 +1,10 @@
 import { PassThrough } from 'stream';
 import fetch, { Response } from 'node-fetch';
 import * as config from '../src/config.ts';
-import Delta from '../src/models/delta';
 import NylasConnection from '../src/nylas-connection';
+import Delta from '../src/models/delta';
+import DeltaCollection from '../src/models/delta-collection';
+import { Deltas } from '../src/models/deltas';
 
 jest.mock('node-fetch', () => {
   const { Request, Response, Headers } = jest.requireActual('node-fetch');
@@ -19,7 +21,7 @@ describe('Delta', () => {
   beforeEach(() => {
     testContext = {};
     testContext.connection = new NylasConnection('123', { clientId: 'foo' });
-    testContext.delta = new Delta(testContext.connection);
+    testContext.delta = new DeltaCollection(testContext.connection);
     jest.useFakeTimers();
   });
 
@@ -199,6 +201,257 @@ describe('Delta', () => {
           expect(e).toEqual('Error.');
           done();
         });
+    });
+  });
+
+  describe('deserialize', () => {
+    test('returns a cursor', done => {
+      const json = {
+        cursor_start: 'start_cursor',
+        cursor_end: 'end_cursor',
+        deltas: [
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              given_name: 'First',
+              surname: 'Last',
+              id: 'id-1234',
+              object: 'contact',
+            },
+            cursor: 'contact_cursor',
+            event: 'create',
+            id: 'delta-1',
+            object: 'contact',
+          },
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              content_type: 'text/plain',
+              filename: 'sample.txt',
+              id: 'id-1234',
+              object: 'file',
+              size: 123,
+            },
+            cursor: 'file_cursor',
+            event: 'create',
+            id: 'delta-2',
+            object: 'file',
+          },
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              to: [{ email: 'foo', name: 'bar' }],
+              subject: 'foo',
+              id: 'id-1234',
+              object: 'message',
+            },
+            cursor: 'message_cursor',
+            event: 'create',
+            id: 'delta-3',
+            object: 'message',
+          },
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              to: [{ email: 'foo', name: 'bar' }],
+              subject: 'foo',
+              id: 'id-1234',
+              object: 'draft',
+            },
+            cursor: 'draft_cursor',
+            event: 'create',
+            id: 'delta-4',
+            object: 'draft',
+          },
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              subject: 'Subject',
+              id: 'id-1234',
+              object: 'thread',
+            },
+            cursor: 'thread_cursor',
+            event: 'create',
+            id: 'delta-5',
+            object: 'thread',
+          },
+          {
+            attributes: {
+              id: 'id-1234',
+              title: 'test event',
+              when: { time: 1409594400, object: 'time' },
+              participants: [
+                {
+                  name: 'foo',
+                  email: 'bar',
+                  status: 'noreply',
+                  comment: 'This is a comment',
+                  phone_number: '416-000-0000',
+                },
+              ],
+              ical_uid: 'id-5678',
+              master_event_id: 'master-1234',
+              original_start_time: 1409592400,
+            },
+            cursor: 'event_cursor',
+            event: 'create',
+            id: 'delta-6',
+            object: 'event',
+          },
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              id: 'id-1234',
+              object: 'folder',
+              name: 'inbox',
+              display_name: 'name',
+            },
+            cursor: 'folder_cursor',
+            event: 'create',
+            id: 'delta-7',
+            object: 'folder',
+          },
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              id: 'id-1234',
+              object: 'label',
+              name: 'inbox',
+            },
+            cursor: 'label_cursor',
+            event: 'create',
+            id: 'delta-8',
+            object: 'label',
+          },
+        ],
+      };
+
+      testContext.connection.request = jest.fn(() => Promise.resolve(json));
+      const deltas = new Deltas(testContext.connection).fromJSON(json);
+
+      expect(deltas.cursorStart).toEqual('start_cursor');
+      expect(deltas.cursorEnd).toEqual('end_cursor');
+      expect(deltas.deltas.length).toBe(8);
+
+      expect(deltas.deltas[0].cursor).toEqual('contact_cursor');
+      expect(deltas.deltas[0].event).toEqual('create');
+      expect(deltas.deltas[0].id).toEqual('delta-1');
+      expect(deltas.deltas[0].object).toEqual('contact');
+      expect(deltas.deltas[0].deltaAttributes.givenName).toEqual('First');
+      expect(deltas.deltas[0].deltaAttributes.surname).toEqual('Last');
+
+      expect(deltas.deltas[1].cursor).toEqual('file_cursor');
+      expect(deltas.deltas[1].event).toEqual('create');
+      expect(deltas.deltas[1].id).toEqual('delta-2');
+      expect(deltas.deltas[1].object).toEqual('file');
+      expect(deltas.deltas[1].deltaAttributes.filename).toEqual('sample.txt');
+      expect(deltas.deltas[1].deltaAttributes.size).toEqual(123);
+      expect(deltas.deltas[1].deltaAttributes.contentType).toEqual(
+        'text/plain'
+      );
+
+      expect(deltas.deltas[2].cursor).toEqual('message_cursor');
+      expect(deltas.deltas[2].event).toEqual('create');
+      expect(deltas.deltas[2].id).toEqual('delta-3');
+      expect(deltas.deltas[2].object).toEqual('message');
+      expect(deltas.deltas[2].deltaAttributes.to[0].email).toEqual('foo');
+      expect(deltas.deltas[2].deltaAttributes.to[0].name).toEqual('bar');
+      expect(deltas.deltas[2].deltaAttributes.subject).toEqual('foo');
+
+      expect(deltas.deltas[3].cursor).toEqual('draft_cursor');
+      expect(deltas.deltas[3].event).toEqual('create');
+      expect(deltas.deltas[3].id).toEqual('delta-4');
+      expect(deltas.deltas[3].object).toEqual('draft');
+      expect(deltas.deltas[3].deltaAttributes.to[0].email).toEqual('foo');
+      expect(deltas.deltas[3].deltaAttributes.to[0].name).toEqual('bar');
+      expect(deltas.deltas[3].deltaAttributes.subject).toEqual('foo');
+
+      expect(deltas.deltas[4].cursor).toEqual('thread_cursor');
+      expect(deltas.deltas[4].event).toEqual('create');
+      expect(deltas.deltas[4].id).toEqual('delta-5');
+      expect(deltas.deltas[4].object).toEqual('thread');
+      expect(deltas.deltas[4].deltaAttributes.subject).toEqual('Subject');
+
+      expect(deltas.deltas[5].cursor).toEqual('event_cursor');
+      expect(deltas.deltas[5].event).toEqual('create');
+      expect(deltas.deltas[5].id).toEqual('delta-6');
+      expect(deltas.deltas[5].object).toEqual('event');
+      expect(deltas.deltas[5].deltaAttributes.participants[0].name).toEqual(
+        'foo'
+      );
+      expect(deltas.deltas[5].deltaAttributes.participants[0].email).toEqual(
+        'bar'
+      );
+      expect(deltas.deltas[5].deltaAttributes.participants[0].status).toEqual(
+        'noreply'
+      );
+      expect(deltas.deltas[5].deltaAttributes.participants[0].comment).toEqual(
+        'This is a comment'
+      );
+      expect(
+        deltas.deltas[5].deltaAttributes.participants[0].phoneNumber
+      ).toEqual('416-000-0000');
+      expect(deltas.deltas[5].deltaAttributes.title).toEqual('test event');
+      expect(deltas.deltas[5].deltaAttributes.when.time).toEqual(1409594400);
+      expect(deltas.deltas[5].deltaAttributes.iCalUID).toEqual('id-5678');
+      expect(deltas.deltas[5].deltaAttributes.masterEventId).toEqual(
+        'master-1234'
+      );
+      expect(deltas.deltas[5].deltaAttributes.originalStartTime).toEqual(
+        new Date(1409592400 * 1000)
+      );
+
+      expect(deltas.deltas[6].cursor).toEqual('folder_cursor');
+      expect(deltas.deltas[6].event).toEqual('create');
+      expect(deltas.deltas[6].id).toEqual('delta-7');
+      expect(deltas.deltas[6].object).toEqual('folder');
+      expect(deltas.deltas[6].deltaAttributes.name).toEqual('inbox');
+      expect(deltas.deltas[6].deltaAttributes.displayName).toEqual('name');
+
+      expect(deltas.deltas[7].cursor).toEqual('label_cursor');
+      expect(deltas.deltas[7].event).toEqual('create');
+      expect(deltas.deltas[7].id).toEqual('delta-8');
+      expect(deltas.deltas[7].object).toEqual('label');
+      expect(deltas.deltas[7].deltaAttributes.name).toEqual('inbox');
+      done();
+    });
+  });
+
+  describe('since', () => {
+    test('returns a cursor', done => {
+      const json = {
+        cursor_start: 'start_cursor',
+        cursor_end: 'end_cursor',
+        deltas: [
+          {
+            attributes: {
+              account_id: 'aid-5678',
+              content_type: 'text/plain',
+              filename: 'sample.txt',
+              id: 'id-1234',
+              object: 'file',
+              size: 123,
+            },
+            cursor: 'file_cursor',
+            event: 'create',
+            id: 'id-1234',
+            object: 'file',
+          },
+        ],
+      };
+
+      testContext.connection.request = jest.fn(() => Promise.resolve(json));
+
+      return testContext.delta.since('end_cursor').then(cursor => {
+        expect(testContext.connection.request).toHaveBeenCalledWith({
+          method: 'GET',
+          path: '/delta',
+          qs: {
+            cursor: 'end_cursor',
+          },
+        });
+        done();
+      });
     });
   });
 });
