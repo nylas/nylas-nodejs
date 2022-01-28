829360ca55b020de9874e1375baa36cb5b9257a1
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-01-28 15:20:14 -0500

    Modify `Delta` to become a `RestfulModel` type

--- src/models/delta.ts
+++ src/models/delta.ts
@@ -1,20 +1,86 @@
 import NylasConnection from '../nylas-connection';
-import DeltaStream from './delta-stream';
+import Attributes, { Attribute } from './attributes';
+import RestfulModel from './restful-model';
+import Contact from './contact';
+import File from './file';
+import Message from './message';
+import Draft from './draft';
+import Thread from './thread';
+import Event from './event';
+import Folder, { Label } from './folder';
 
-export type LatestCursor = {
-  cursor: string;
+const DeltaClassMap: Record<string, typeof RestfulModel> = Object.freeze({
+  contact: Contact,
+  file: File,
+  message: Message,
+  draft: Draft,
+  thread: Thread,
+  event: Event,
+  folder: Folder,
+  label: Label,
+});
+
+export type DeltaParams = {
+  view?: string;
+  includeTypes?: string[];
+  excludeTypes?: string[];
+  expanded?: boolean;
 };
 
-export default class Delta {
-  connection: NylasConnection;
-  static streamingTimeoutMs = 15000;
+export type DeltaProperties = {
+  id: string;
+  cursor: string;
+  event: string;
+  object: string;
+  deltaAttributes?: unknown;
+};
 
-  constructor(connection: NylasConnection) {
+export default class Delta extends RestfulModel implements DeltaProperties {
+  id = '';
+  cursor = '';
+  event = '';
+  object = '';
+  connection: NylasConnection;
+  deltaAttributes?: RestfulModel;
+  static streamingTimeoutMs = 15000;
+  static attributes: Record<string, Attribute> = {
+    id: Attributes.String({
+      modelKey: 'id',
+    }),
+    cursor: Attributes.String({
+      modelKey: 'cursor',
+    }),
+    event: Attributes.String({
+      modelKey: 'event',
+    }),
+    object: Attributes.String({
+      modelKey: 'object',
+    }),
+    deltaAttributes: Attributes.Object({
+      modelKey: 'deltaAttributes',
+      jsonKey: 'attributes',
+    }),
+  };
+
+  constructor(connection: NylasConnection, props?: DeltaProperties) {
+    super(connection);
     this.connection = connection;
-    if (!(this.connection instanceof NylasConnection)) {
-      throw new Error('Connection object not provided');
+    this.initAttributes(props);
+    if (this.deltaAttributes && DeltaClassMap[this.object]) {
+      this.deltaAttributes = new DeltaClassMap[this.object](
+        connection,
+        this.deltaAttributes
+      );
     }
   }
 
+  fromJSON(json: Record<string, unknown>): this {
+    super.fromJSON(json);
+    if (this.deltaAttributes && DeltaClassMap[this.object]) {
+      this.deltaAttributes = new DeltaClassMap[this.object](
+        this.connection
+      ).fromJSON(this.deltaAttributes);
+    }
+    return this;
   }
 }
