75e27088625f6cdecf2b4597213abc9abaddcd93
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-01-28 15:19:33 -0500

    Move functions in `Delta` into new `DeltaCollection`
    `Delta` should represent a `Delta` object, and instead these functions should move into a 'Collection' class instead

--- src/models/delta.ts
+++ src/models/delta.ts
@@ -16,36 +16,5 @@ export default class Delta {
     }
   }
 
-  latestCursor(
-    callback: (error: Error | null, cursor: string | null) => void
-  ): Promise<string> {
-    const reqOpts = {
-      method: 'POST',
-      path: '/delta/latest_cursor',
-    };
-
-    return this.connection
-      .request(reqOpts)
-      .then((response: LatestCursor) => {
-        if (callback) {
-          callback(null, response.cursor);
-        }
-        return Promise.resolve(response.cursor);
-      })
-      .catch(err => {
-        if (callback) {
-          callback(err, null);
-        }
-        return Promise.reject(err);
-      });
-  }
-
-  async startStream(
-    cursor: string,
-    params: Record<string, unknown> = {}
-  ): Promise<DeltaStream> {
-    const stream = new DeltaStream(this.connection, cursor, params);
-    await stream.open();
-    return stream;
   }
 }
