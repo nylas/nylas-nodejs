fc5526a3949069e716b3657338f20d6e872b4b6f
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-02-03 12:23:43 -0500

    Add new model representing Webhook Notifications
    This isn't going to be utilized within the SDK at the moment, but it provides typing and a class to instantiate webhook deltas from

---
+++ src/models/webhook-notification.ts
@@ -0,0 +1,294 @@
+import Model from './model';
+import Attributes, { Attribute } from './attributes';
+
+export type LinkClickProperties = {
+  id: number;
+  ip: string;
+  userAgent: string;
+  timestamp: Date;
+  linkIndex?: number;
+}
+
+export class LinkClick extends Model implements LinkClickProperties {
+  id = 0;
+  ip = '';
+  userAgent = '';
+  timestamp = new Date();
+  linkIndex?: number;
+  static attributes: Record<string, Attribute> = {
+    id: Attributes.Number({
+      modelKey: 'id',
+    }),
+    ip: Attributes.String({
+      modelKey: 'ip',
+    }),
+    userAgent: Attributes.String({
+      modelKey: 'userAgent',
+      jsonKey: 'user_agent',
+    }),
+    timestamp: Attributes.DateTime({
+      modelKey: 'timestamp',
+    }),
+    linkIndex: Attributes.Number({
+      modelKey: 'linkIndex',
+      jsonKey: 'link_index',
+    }),
+  };
+
+  constructor(props?: LinkClickCountProperties) {
+    super();
+    this.initAttributes(props);
+  }
+}
+
+export type LinkClickCountProperties = {
+  url: string;
+  count: number;
+}
+
+export class LinkClickCount extends Model implements LinkClickCountProperties {
+  url = '';
+  count = 0;
+  static attributes: Record<string, Attribute> = {
+    url: Attributes.String({
+      modelKey: 'url',
+    }),
+    count: Attributes.Number({
+      modelKey: 'count',
+    }),
+  };
+
+  constructor(props?: LinkClickCountProperties) {
+    super();
+    this.initAttributes(props);
+  }
+}
+
+export type MessageTrackingDataProperties = {
+  messageId: string;
+  payload: string;
+  senderAppId: number;
+  threadId?: string;
+  replyToMessageId?: string;
+  timestamp?: number;
+  count?: number;
+  fromSelf?: boolean;
+  recents?: LinkClickProperties[];
+  linkData?: LinkClickCountProperties[];
+}
+
+export class MessageTrackingData extends Model
+  implements MessageTrackingDataProperties {
+  messageId = '';
+  payload = '';
+  senderAppId = 0;
+
+  // thread.replied specific fields
+  replyToMessageId?: string;
+  timestamp?: number;
+  threadId?: string;
+  fromSelf?: boolean;
+
+  // message.opened specific field
+  count?: number;
+
+  // message.link_clicked specific fields
+  linkData?: LinkClickCount[];
+
+  // message.opened and message.link_clicked shared field
+  recents?: LinkClick[];
+  static attributes: Record<string, Attribute> = {
+    messageId: Attributes.String({
+      modelKey: 'messageId',
+      jsonKey: 'message_id',
+    }),
+    payload: Attributes.String({
+      modelKey: 'payload',
+    }),
+    senderAppId: Attributes.Number({
+      modelKey: 'senderAppId',
+      jsonKey: 'sender_app_id',
+    }),
+    replyToMessageId: Attributes.String({
+      modelKey: 'replyToMessageId',
+      jsonKey: 'reply_to_message_id'
+    }),
+    timestamp: Attributes.Number({
+      modelKey: 'timestamp',
+    }),
+    threadId: Attributes.String({
+      modelKey: 'threadId',
+      jsonKey: 'thread_id'
+    }),
+    fromSelf: Attributes.Boolean({
+      modelKey: 'fromSelf',
+      jsonKey: 'from_self',
+    }),
+    count: Attributes.Number({
+      modelKey: 'count',
+    }),
+    linkData: Attributes.Collection({
+      modelKey: 'linkData',
+      jsonKey: 'link_data',
+      itemClass: LinkClickCount,
+    }),
+    recents: Attributes.Collection({
+      modelKey: 'recents',
+      itemClass: LinkClick,
+    }),
+  };
+
+  constructor(props?: MessageTrackingDataProperties) {
+    super();
+    this.initAttributes(props);
+  }
+}
+
+export type WebhookObjectAttributesProperties = {
+  action?: string;
+  jobStatusId?: string;
+  threadId?: string;
+  receivedDate?: Date;
+}
+
+export class WebhookObjectAttributes extends Model
+  implements WebhookObjectAttributesProperties {
+  // Job Status specific fields
+  action?: string;
+  jobStatusId?: string;
+
+  // Message specific fields
+  threadId?: string;
+  receivedDate?: Date;
+  static attributes: Record<string, Attribute> = {
+    action: Attributes.String({
+      modelKey: 'action',
+    }),
+    jobStatusId: Attributes.String({
+      modelKey: 'jobStatusId',
+      jsonKey: 'job_status_id',
+    }),
+    threadId: Attributes.String({
+      modelKey: 'threadId',
+      jsonKey: 'thread_id',
+    }),
+    receivedDate: Attributes.DateTime({
+      modelKey: 'receivedDate',
+      jsonKey: 'received_date',
+    }),
+  };
+
+  constructor(props?: WebhookObjectAttributesProperties) {
+    super();
+    this.initAttributes(props);
+  }
+}
+
+export type WebhookObjectDataProperties = {
+  id: string;
+  accountId: string;
+  namespaceId: string;
+  object: string;
+  metadata?: MessageTrackingDataProperties;
+  objectAttributes?: WebhookObjectAttributesProperties;
+}
+
+export class WebhookObjectData extends Model
+  implements WebhookObjectDataProperties {
+  id = '';
+  accountId = '';
+  namespaceId = '';
+  object = '';
+
+  // Message specific field
+  metadata?: MessageTrackingData;
+
+  // Message and Job Status specific field
+  objectAttributes?: WebhookObjectAttributes;
+  static attributes: Record<string, Attribute> = {
+    id: Attributes.String({
+      modelKey: 'id',
+    }),
+    accountId: Attributes.String({
+      modelKey: 'accountId',
+      jsonKey: 'account_id',
+    }),
+    namespaceId: Attributes.String({
+      modelKey: 'namespaceId',
+      jsonKey: 'namespace_id',
+    }),
+    object: Attributes.String({
+      modelKey: 'object',
+    }),
+    metadata: Attributes.Object({
+      modelKey: 'metadata',
+      itemClass: MessageTrackingData,
+    }),
+    objectAttributes: Attributes.Object({
+      modelKey: 'objectAttributes',
+      jsonKey: 'attributes',
+      itemClass: WebhookObjectAttributes,
+    }),
+  };
+
+  constructor(props?: WebhookObjectDataProperties) {
+    super();
+    this.initAttributes(props);
+  }
+}
+
+export type WebhookDeltaProperties = {
+  object: string;
+  type: string;
+  date: Date;
+  objectData: WebhookObjectDataProperties;
+}
+
+export class WebhookDelta extends Model
+  implements WebhookDeltaProperties {
+  date = new Date();
+  object = '';
+  type = '';
+  objectData = new WebhookObjectData();
+  static attributes: Record<string, Attribute> = {
+    date: Attributes.DateTime({
+      modelKey: 'date',
+    }),
+    object: Attributes.String({
+      modelKey: 'object',
+    }),
+    type: Attributes.String({
+      modelKey: 'type',
+    }),
+    objectData: Attributes.Object({
+      modelKey: 'objectData',
+      jsonKey: 'object_data',
+      itemClass: WebhookObjectData,
+    }),
+  }
+
+  constructor(props?: WebhookDeltaProperties) {
+    super();
+    this.initAttributes(props);
+  }
+}
+
+export type WebhookNotificationProperties = {
+  deltas: WebhookDeltaProperties[];
+}
+
+export default class WebhookNotification extends Model
+  implements WebhookNotificationProperties {
+  deltas: WebhookDelta[] = [];
+  static attributes: Record<string, Attribute> = {
+    deltas: Attributes.Collection({
+      modelKey: 'deltas',
+      itemClass: WebhookDelta,
+    }),
+  };
+
+  constructor(props?: WebhookNotificationProperties) {
+    super();
+    this.initAttributes(props);
+  }
+}
