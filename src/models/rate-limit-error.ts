8e7b6abc91ef08f1a0ce52e51fcb82f34dbd7e26
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-11-04 17:49:17 -0400

    Introduce model for 429 errors

---
+++ src/models/rate-limit-error.ts
@@ -0,0 +1,23 @@
+import NylasApiError from './nylas-api-error';
+import { Headers } from 'node-fetch';
+
+export default class RateLimitError extends NylasApiError {
+  static RATE_LIMIT_STATUS_CODE = 429;
+  static RATE_LIMIT_LIMIT_HEADER = 'X-RateLimit-Limit';
+  static RATE_LIMIT_RESET_HEADER = 'X-RateLimit-Reset';
+
+  rateLimit?: number;
+  rateLimitReset?: number;
+
+  constructor(
+    type: string,
+    message: string,
+    rateLimit?: number,
+    rateLimitReset?: number
+  ) {
+    super(RateLimitError.RATE_LIMIT_STATUS_CODE, type, message);
+    this.rateLimit = rateLimit;
+    this.rateLimitReset = rateLimitReset;
+  }
+  }
+}
