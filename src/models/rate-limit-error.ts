5c99d005a3dd7107af2bd2905ae4a7c8b80cc158
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-11-08 12:45:23 -0500

    Add documentation for RateLimitError

--- src/models/rate-limit-error.ts
+++ src/models/rate-limit-error.ts
@@ -1,12 +1,23 @@
 import NylasApiError from './nylas-api-error';
 import { Headers } from 'node-fetch';
 
+/**
+ * This error class represents a 429 error response, with details on the rate limit
+ */
+
 export default class RateLimitError extends NylasApiError {
   static RATE_LIMIT_STATUS_CODE = 429;
   static RATE_LIMIT_LIMIT_HEADER = 'X-RateLimit-Limit';
   static RATE_LIMIT_RESET_HEADER = 'X-RateLimit-Reset';
 
+  /**
+   * The rate limit
+   */
   rateLimit?: number;
+
+  /**
+   * The rate limit expiration time, in seconds
+   */
   rateLimitReset?: number;
 
   constructor(
@@ -20,6 +31,12 @@ export default class RateLimitError extends NylasApiError {
     this.rateLimitReset = rateLimitReset;
   }
 
+  /**
+   * Parses an API response and generates a 429 error with details filled in
+   * @param parsedApiError The response parsed as a JSON
+   * @param headers The response headers
+   * @return The error with the rate limit details filled in
+   */
   static parseErrorResponse(
     parsedApiError: Record<string, string>,
     headers: Headers
