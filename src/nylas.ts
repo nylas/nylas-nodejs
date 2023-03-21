677072912e978d9bf63f8e1fd31f9deb2177b817
Albert T <albert.t@nylas.com>
2023-03-21 16:28:23 -0400

    instantiate calendars

--- src/nylas.ts
+++ src/nylas.ts
@@ -1,21 +1,29 @@
+import APIClient from './apiClient';
 import { REGION_CONFIG, DEFAULT_REGION, NylasConfig } from './config';
+import { Calendars } from './resources/calendars';
 
-class Nylas {
+export class Nylas {
   serverUrl = REGION_CONFIG[DEFAULT_REGION].nylasAPIUrl;
+  apiClient: APIClient;
 
   // TODO: remove from config?
   clientId?: string;
   clientSecret?: string;
+
+  public calendars: Calendars;
 
   constructor(config: NylasConfig) {
     if (config.serverUrl) {
       this.serverUrl = config.serverUrl;
     }
 
-    // TODO: instantiate resources
+    this.apiClient = new APIClient({
+      apiKey: config.apiKey,
+      serverUrl: this.serverUrl,
+    });
+
+    this.calendars = new Calendars(this.apiClient);
 
     return this;
   }
 }
-
-export = Nylas;
