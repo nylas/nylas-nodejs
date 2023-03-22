b55d04c8be8cbde75e114fda848f949f0727190a
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-03-22 10:21:11 -0400

    add generalized casing function & add snake_case

--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,39 @@
-import { camelCase } from 'change-case';
+import { camelCase, snakeCase } from 'change-case';
 
-// write a function that recursively converts all keys in an object to camelCase
-export function objKeysToCamelCase(
-  obj: Record<string, unknown>
-): Record<string, unknown> {
+type CasingFunction = (input: string, options?: any) => string;
+
+function convertCase(
+  obj: Record<string, unknown>,
+  casingFunction: CasingFunction
+) {
   const newObj = {} as Record<string, unknown>;
   for (const key in obj) {
     if (Array.isArray(obj[key])) {
-      newObj[camelCase(key)] = (obj[key] as any[]).map(item =>
-        objKeysToCamelCase(item)
+      newObj[casingFunction(key)] = (obj[key] as any[]).map(item =>
+        convertCase(item, casingFunction)
       );
     } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-      newObj[camelCase(key)] = objKeysToCamelCase(
-        obj[key] as Record<string, unknown>
+      newObj[casingFunction(key)] = convertCase(
+        obj[key] as Record<string, unknown>,
+        casingFunction
       );
     } else {
-      newObj[camelCase(key)] = obj[key];
+      newObj[casingFunction(key)] = obj[key];
     }
   }
   return newObj;
+}
+
+// function that recursively converts all keys in an object to camelCase
+export function objKeysToCamelCase(
+  obj: Record<string, unknown>
+): Record<string, unknown> {
+  return convertCase(obj, camelCase);
+}
+
+// function that recursively converts all keys in an object to snake_case
+export function objKeysToSnakeCase(
+  obj: Record<string, unknown>
+): Record<string, unknown> {
+  return convertCase(obj, snakeCase);
 }
