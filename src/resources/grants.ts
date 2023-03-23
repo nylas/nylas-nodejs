11c17c23ce636efdb3eb3b4c1ecdee3235f063c3
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-03-23 16:53:23 -0400

    Add list Grant support

--- src/resources/grants.ts
+++ src/resources/grants.ts
@@ -1,3 +1,22 @@
 import { BaseResource } from './baseResource';
+import { Overrides } from '../config';
+import { List } from '../schema/response';
+import { Grant, GrantSchema, ListGrantsQueryParams } from '../schema/grants';
 
-export class Grants extends BaseResource {}
+interface ListGrantsParams {
+  queryParams?: ListGrantsQueryParams;
+}
+
+export class Grants extends BaseResource {
+  public async list({
+    queryParams,
+    overrides,
+  }: ListGrantsParams & Overrides): Promise<List<Grant>> {
+    return super._list<Grant>({
+      queryParams,
+      path: `/v3/grants`,
+      overrides,
+      responseSchema: GrantSchema,
+    });
+  }
+}
