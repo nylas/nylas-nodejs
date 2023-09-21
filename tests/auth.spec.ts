479fa84f9291df0ba7506063d69afa843f814498
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-09-21 16:38:45 -0400

    AV-825 fix auth tests

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -2,11 +2,9 @@ import APIClient from '../src/apiClient';
 import { Auth } from '../src/resources/auth';
 import {
   CodeExchangeRequest,
-  ServerSideHostedAuthRequest,
   TokenExchangeRequest,
 } from '../src/models/auth.js';
-import sha256 from 'sha256';
-jest.mock('uuid', () => ({ v4: (): string => '123456789' }));
+jest.mock('uuid', () => ({ v4: (): string => 'nylas' }));
 
 describe('Auth', () => {
   let apiClient: APIClient;
@@ -19,7 +17,7 @@ describe('Auth', () => {
       timeout: 30,
     });
 
-    auth = new Auth(apiClient, 'clientId', 'clientSecret');
+    auth = new Auth(apiClient);
     jest.spyOn(APIClient.prototype, 'request').mockResolvedValue({});
   });
 
@@ -27,6 +25,8 @@ describe('Auth', () => {
     describe('exchangeCodeForToken', () => {
       it('should call apiClient.request with the correct params', async () => {
         const payload: CodeExchangeRequest = {
+          clientId: 'clientId',
+          clientSecret: 'clientSecret',
           redirectUri: 'https://redirect.uri/path',
           code: 'code',
         };
@@ -47,6 +47,8 @@ describe('Auth', () => {
 
       it('should set codeVerifier', async () => {
         const payload: CodeExchangeRequest = {
+          clientId: 'clientId',
+          clientSecret: 'clientSecret',
           redirectUri: 'https://redirect.uri/path',
           code: 'code',
           codeVerifier: 'codeVerifier',
@@ -71,6 +73,8 @@ describe('Auth', () => {
     describe('refreshAccessToken', () => {
       it('should call apiClient.request with the correct params', async () => {
         const payload: TokenExchangeRequest = {
+          clientId: 'clientId',
+          clientSecret: 'clientSecret',
           redirectUri: 'https://redirect.uri/path',
           refreshToken: 'refreshToken',
         };
@@ -90,39 +94,40 @@ describe('Auth', () => {
       });
     });
   });
-  describe('Validating token', () => {
-    describe('validateIDToken', () => {
-      it('should call apiClient.request with the correct params', async () => {
-        await auth.validateIDToken('id123');
-
-        expect(apiClient.request).toHaveBeenCalledWith({
-          method: 'GET',
-          path: '/v3/connect/tokeninfo',
-          queryParams: {
-            idToken: 'id123',
-          },
-        });
-      });
-    });
-
-    describe('validateAccessToken', () => {
-      it('should call apiClient.request with the correct params', async () => {
-        await auth.validateAccessToken('accessToken123');
-
-        expect(apiClient.request).toHaveBeenCalledWith({
-          method: 'GET',
-          path: '/v3/connect/tokeninfo',
-          queryParams: {
-            accessToken: 'accessToken123',
-          },
-        });
-      });
-    });
-  });
+  // describe('Validating token', () => {
+  //   describe('validateIDToken', () => {
+  //     it('should call apiClient.request with the correct params', async () => {
+  //       await auth.validateIDToken('id123');
+  //
+  //       expect(apiClient.request).toHaveBeenCalledWith({
+  //         method: 'GET',
+  //         path: '/v3/connect/tokeninfo',
+  //         queryParams: {
+  //           idToken: 'id123',
+  //         },
+  //       });
+  //     });
+  //   });
+  //
+  //   describe('validateAccessToken', () => {
+  //     it('should call apiClient.request with the correct params', async () => {
+  //       await auth.validateAccessToken('accessToken123');
+  //
+  //       expect(apiClient.request).toHaveBeenCalledWith({
+  //         method: 'GET',
+  //         path: '/v3/connect/tokeninfo',
+  //         queryParams: {
+  //           accessToken: 'accessToken123',
+  //         },
+  //       });
+  //     });
+  //   });
+  // });
   describe('URL building', () => {
     describe('urlForAuthentication', () => {
       it('should build the correct url', () => {
-        const url = auth.urlForAuthentication({
+        const url = auth.urlForOAuth2({
+          clientId: 'clientId',
           redirectUri: 'https://redirect.uri/path',
           scope: ['calendar'],
           provider: 'google',
@@ -135,41 +140,40 @@ describe('Auth', () => {
       });
 
       it('should build the correct url if all the fields are set', () => {
-        const url = auth.urlForAuthentication({
+        const url = auth.urlForOAuth2({
+          clientId: 'clientId',
           redirectUri: 'https://redirect.uri/path',
           scope: ['calendar'],
           provider: 'google',
           loginHint: 'loginHint',
           includeGrantScopes: true,
           prompt: 'prompt',
-          metadata: 'metadata',
           state: 'state',
           accessType: 'online',
         });
 
         expect(url).toBe(
-          'https://test.api.nylas.com/v3/connect/auth?client_id=clientId&redirect_uri=https%3A%2F%2Fredirect.uri%2Fpath&access_type=online&response_type=code&provider=google&login_hint=loginHint&include_grant_scopes=true&scope=calendar&prompt=prompt&metadata=metadata&state=state'
+          'https://test.api.nylas.com/v3/connect/auth?client_id=clientId&redirect_uri=https%3A%2F%2Fredirect.uri%2Fpath&access_type=online&response_type=code&provider=google&login_hint=loginHint&include_grant_scopes=true&scope=calendar&prompt=prompt&state=state'
         );
       });
     });
 
     describe('urlForAuthenticationPKCE', () => {
       it('should hash the secret and build the URL correctly', () => {
-        const pkce = auth.urlForAuthenticationPKCE({
+        const pkce = auth.urlForOAuth2PKCE({
+          clientId: 'clientId',
           redirectUri: 'https://redirect.uri/path',
           scope: ['calendar'],
           provider: 'google',
           includeGrantScopes: true,
         });
-        const codeChallenge = Buffer.from(sha256('123456789')).toString(
-          'base64'
-        );
+        const codeChallenge =
+          'ZTk2YmY2Njg2YTNjMzUxMGU5ZTkyN2RiNzA2OWNiMWNiYTliOTliMDIyZjQ5NDgzYTZjZTMyNzA4MDllNjhhMg';
 
         expect(pkce).toEqual({
-          secret: '123456789',
+          secret: 'nylas',
           secretHash: codeChallenge,
-          url:
-            'https://test.api.nylas.com/v3/connect/auth?client_id=clientId&redirect_uri=https%3A%2F%2Fredirect.uri%2Fpath&access_type=online&response_type=code&provider=google&scope=calendar&code_challenge_method=s256&code_challenge=123456789',
+          url: `https://test.api.nylas.com/v3/connect/auth?client_id=clientId&redirect_uri=https%3A%2F%2Fredirect.uri%2Fpath&access_type=online&response_type=code&provider=google&scope=calendar&code_challenge_method=s256&code_challenge=${codeChallenge}`,
         });
       });
     });
@@ -177,45 +181,19 @@ describe('Auth', () => {
     describe('urlForAdminConsent', () => {
       it('should build the correct url', () => {
         const url = auth.urlForAdminConsent({
+          clientId: 'clientId',
           redirectUri: 'https://redirect.uri/path',
           scope: ['calendar'],
           loginHint: 'loginHint',
           includeGrantScopes: true,
           prompt: 'prompt',
-          metadata: 'metadata',
           state: 'state',
           credentialId: 'credentialId',
         });
 
         expect(url).toBe(
-          'https://test.api.nylas.com/v3/connect/auth?client_id=clientId&redirect_uri=https%3A%2F%2Fredirect.uri%2Fpath&access_type=online&response_type=adminconsent&provider=microsoft&login_hint=loginHint&include_grant_scopes=true&scope=calendar&prompt=prompt&metadata=metadata&state=state&credential_id=credentialId'
+          'https://test.api.nylas.com/v3/connect/auth?client_id=clientId&redirect_uri=https%3A%2F%2Fredirect.uri%2Fpath&access_type=online&response_type=adminconsent&provider=microsoft&login_hint=loginHint&include_grant_scopes=true&scope=calendar&prompt=prompt&state=state&credential_id=credentialId'
         );
-      });
-    });
-  });
-  describe('hostedAuth', () => {
-    it('should call apiClient.request with the correct params', async () => {
-      const hostedAuthRequest: ServerSideHostedAuthRequest = {
-        redirectUri: 'https://redirect.uri/path',
-        state: 'state',
-        loginHint: 'loginHint',
-        cookieNonce: 'nonce',
-        scope: ['calendar'],
-        provider: 'google',
-      };
-      await auth.serverSideHostedAuth(hostedAuthRequest);
-
-      expect(apiClient.request).toHaveBeenCalledWith({
-        method: 'POST',
-        path: '/v3/connect/auth',
-        body: {
-          redirectUri: 'https://redirect.uri/path',
-          state: 'state',
-          loginHint: 'loginHint',
-          cookieNonce: 'nonce',
-          scope: ['calendar'],
-          provider: 'google',
-        },
       });
     });
   });
@@ -230,21 +208,6 @@ describe('Auth', () => {
           token: 'accessToken123',
         },
       });
-    });
-  });
-  describe('checkAuthCredentials', () => {
-    it('should throw an error if no clientId', () => {
-      const authNoClientId = new Auth(apiClient, '', 'clientSecret');
-      expect(() => authNoClientId.urlForAuthentication({} as any)).toThrowError(
-        'ClientID & ClientSecret are required for using auth'
-      );
-    });
-
-    it('should throw an error if no clientSecret', () => {
-      const authNoClientSecret = new Auth(apiClient, 'clientId', '');
-      expect(() =>
-        authNoClientSecret.urlForAuthentication({} as any)
-      ).toThrowError('ClientID & ClientSecret are required for using auth');
     });
   });
 });
