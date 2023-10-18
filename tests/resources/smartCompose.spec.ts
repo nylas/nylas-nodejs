c3aa83afb7110979eeadb924537913e3e599604c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-10-18 12:34:45 -0400

    add smart compose test cases

---
+++ tests/resources/smartCompose.spec.ts
@@ -0,0 +1,70 @@
+import APIClient from '../../src/apiClient';
+import { SmartCompose } from '../../src/resources/smartCompose';
+jest.mock('../src/apiClient');
+
+describe('SmartCompose', () => {
+  let apiClient: jest.Mocked<APIClient>;
+  let smartCompose: SmartCompose;
+
+  beforeAll(() => {
+    apiClient = new APIClient({
+      apiKey: 'apiKey',
+      apiUri: 'https://test.api.nylas.com',
+      timeout: 30,
+    }) as jest.Mocked<APIClient>;
+
+    smartCompose = new SmartCompose(apiClient);
+    apiClient.request.mockResolvedValue({});
+  });
+
+  describe('composeMessage', () => {
+    it('should call apiClient.request with the correct params', async () => {
+      await smartCompose.composeMessage({
+        identifier: 'id123',
+        requestBody: {
+          prompt: 'This is an example prompt',
+        },
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+        },
+      });
+
+      expect(apiClient.request).toHaveBeenCalledWith({
+        method: 'POST',
+        path: '/v3/grants/id123/messages/smart-compose',
+        body: {
+          prompt: 'This is an example prompt',
+        },
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+        },
+      });
+    });
+  });
+
+  describe('composeMessageReply', () => {
+    it('should call apiClient.request with the correct params', async () => {
+      await smartCompose.composeMessageReply({
+        identifier: 'id123',
+        messageId: 'message123',
+        requestBody: {
+          prompt: 'This is an example prompt',
+        },
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+        },
+      });
+
+      expect(apiClient.request).toHaveBeenCalledWith({
+        method: 'POST',
+        path: '/v3/grants/id123/messages/message123/smart-compose',
+        body: {
+          prompt: 'This is an example prompt',
+        },
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+        },
+      });
+    });
+  });
+});
