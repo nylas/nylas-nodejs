a7d720b603d677df163402b184cfa58f4367491f
Subash Pradhan <subash.p@macbook-pro.home>
2024-10-21 14:13:58 +0200

    Add test cases for session endpoint

---
+++ tests/resources/sessions.spec.ts
@@ -0,0 +1,69 @@
+import APIClient from "../../src/apiClient";
+import { Sessions } from "../../src/resources/sessions";
+jest.mock('../src/apiClient')
+
+describe('Sessions', () => {
+  let apiClient: jest.Mocked<APIClient>
+  let sessions: Sessions
+
+  beforeAll(() => {
+    apiClient = new APIClient({
+      apiKey: 'apiKey',
+      apiUri: 'https://test.api.nylas.com',
+      timeout: 30,
+      headers: {},
+    }) as jest.Mocked<APIClient>;
+
+    sessions = new Sessions(apiClient);
+    apiClient.request.mockResolvedValue({});
+  });
+
+  describe('create', () => {
+    it('should call apiClient.request with the correct params', async () => {
+      await sessions.create({
+        requestBody: {
+          "configurationId": "configuration123",
+          "timeToLive": 30
+        },
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+          headers: { override: 'foobar' },
+        },
+      })
+
+      expect(apiClient.request).toHaveBeenCalledWith({
+        method: 'POST',
+        path: '/v3/scheduling/sessions',
+        body: {
+          "configurationId": "configuration123",
+          "timeToLive": 30
+        },
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+          headers: { override: 'foobar' },
+        }
+      })
+    })
+  })
+
+  describe('destroy', () => {
+    it('should call apiClient.request with the correct params', async () => {
+      await sessions.destroy({
+        sessionId: 'session123',
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+          headers: { override: 'foobar' },
+        }
+      })
+
+      expect(apiClient.request).toHaveBeenCalledWith({
+        method: 'DELETE',
+        path: '/v3/scheduling/sessions/session123',
+        overrides: {
+          apiUri: 'https://test.api.nylas.com',
+          headers: { override: 'foobar' },
+        }
+      })
+    })
+  })
+})
