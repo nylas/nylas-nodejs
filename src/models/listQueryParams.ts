1391ed46336c6680b09a60556a13320019c7ec77
Subash Pradhan <subash.p@macbook-pro.home>
2024-10-31 10:52:46 +0100

    lint

--- src/models/listQueryParams.ts
+++ src/models/listQueryParams.ts
@@ -7,12 +7,12 @@ export interface ListQueryParams {
   /**
    * An identifier that specifies which page of data to return.
    * This value should be taken from the [ListResponse.nextCursor] response field.
-  */
+   */
   pageToken?: string;
   /**
    * Specify fields that you want Nylas to return
-   * as a comma-separated list (for example, select=id,updated_at). 
-   * This allows you to receive only the portion of object data that you're interested in. 
-  */
+   * as a comma-separated list (for example, select=id,updated_at).
+   * This allows you to receive only the portion of object data that you're interested in.
+   */
   select?: string;
 }
